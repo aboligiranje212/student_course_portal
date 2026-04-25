1b273bbdf406b64df9d433b3435f4cd6f3257599
aboligiranje <aboligiranjecgpi@gmail.com>
2026-04-25 16:48:48 +0530

    remove

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ class App extends Component {
         <Switch>
           <Route
             exact
-            path="/student-course-portal/'"
+            path="/'"
             render={(props) => (
               <CourseList {...props} courses={this.state.courses} />
             )}

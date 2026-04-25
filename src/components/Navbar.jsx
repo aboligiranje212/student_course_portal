042f48018f5a276b52d9b046aef14d39d034ad53
aboligiranje <aboligiranjecgpi@gmail.com>
2026-04-25 16:38:06 +0530

    update repository path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ class Navbar extends Component {
     return (
       <nav className="navbar navbar-dark bg-dark px-3">
         <span className="navbar-brand mb-0 h1">Course Portal</span>
-        <Link to="/" className="btn btn-outline-light">Home</Link>
+        <Link to="/'" className="btn btn-outline-light">Home</Link>
       </nav>
     );
   }
